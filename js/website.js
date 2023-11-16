ccb8560851932810d10e1d4e3be6cf669ad47c09
Christian Escano <escanochristian21@gmail.com>
2023-11-16 14:29:44 -0500

    Changed executive media scaling; ;

--- js/website.js
+++ js/website.js
@@ -5,16 +5,16 @@ const imageInfo = [
     {url: "images/technology_3DIC-710x480.jpg", id: "three-d-fabric-img", scale: 0.5},
     {url: "images/technology_platform-710x480.jpg", id: "tech-platforms-img", scale: 0.5},
     {url: "images/TSMC_Organization-1500x1636.png", id: "org-chart", scale: 0.5},
-    {url: "images/executives1.png", id: "executive1", scale: 0.6},
-    {url: "images/executives2.png", id: "executive2", scale: 0.6},
-    {url: "images/executives3.png", id: "executive3", scale: 0.6},
-    {url: "images/executives4.png", id: "executive4", scale: 0.6},
-    {url: "images/executives5.png", id: "executive5", scale: 0.6},
-    {url: "images/executives6.png", id: "executive6", scale: 0.6},
-    {url: "images/executives7.png", id: "executive7", scale: 0.6},
-    {url: "images/executives8.png", id: "executive8", scale: 0.6},
-    {url: "images/executives9.png", id: "executive9", scale: 0.6},
-    {url: "images/executives10.png", id: "executive10", scale: 0.6}
+    {url: "images/executives1.png", id: "executive1", scale: 0.55},
+    {url: "images/executives2.png", id: "executive2", scale: 0.55},
+    {url: "images/executives3.png", id: "executive3", scale: 0.55},
+    {url: "images/executives4.png", id: "executive4", scale: 0.55},
+    {url: "images/executives5.png", id: "executive5", scale: 0.55},
+    {url: "images/executives6.png", id: "executive6", scale: 0.55},
+    {url: "images/executives7.png", id: "executive7", scale: 0.55},
+    {url: "images/executives8.png", id: "executive8", scale: 0.55},
+    {url: "images/executives9.png", id: "executive9", scale: 0.55},
+    {url: "images/executives10.png", id: "executive10", scale: 0.55}
 ]
 
 /*-----Functions-----*/
